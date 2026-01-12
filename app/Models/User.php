22e2014ddbeda20186212ee5385f67ccb7a34992
Manuel <barcelonamanuelmarc@gmail.com>
2026-01-12 21:04:09 +0800

    modified user.php

--- app/Models/User.php
+++ app/Models/User.php
@@ -12,7 +12,7 @@ use Laravel\Fortify\TwoFactorAuthenticatable;
 class User extends Authenticatable
 {
     // echo "// Testing CI" 
-    // Trello lite website for personal project, hope this succeeds.
+    // Trello lite website for personal project, hope this succeedsaaaaaaaaa.
     /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable, TwoFactorAuthenticatable;
 
