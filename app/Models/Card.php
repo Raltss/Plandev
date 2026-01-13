db30e981fff6c7f8a33d17bb9128ff3792d6eda0
Manuel <barcelonamanuelmarc@gmail.com>
2026-01-13 23:26:41 +0800

    MAJOR CHANGES
    Added edit/update of cards
    
    Added due date to cards
    
    Refactored welcome page

--- app/Models/Card.php
+++ app/Models/Card.php
@@ -6,8 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Card extends Model
 {
-    protected $fillable = ['board_list_id', 'title', 'description', 'position'];
+    protected $fillable = ['title', 'description', 'board_list_id', 'due_date', 'position'];
 
+    protected $casts = [
+        'due_date' => 'datetime',
+    ];
     public function boardList()
     {
         return $this->belongsTo(BoardList::class);
